84f76e94ed58c42ce3dba6a997337de5bd6439cd
Konstantine <ttes7a@gmail.com>
2025-02-27 13:34:17 +0200

    fixed cors

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import { Request, Response } from "express";
 import db from "./db.json";
 
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const port = 3001;
+app.use(cors());
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
